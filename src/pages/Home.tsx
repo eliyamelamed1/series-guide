13bfd687a410cbb78f2d2cc504fa7f09e08d570c
eliyamelamed1 <eliyamelamed1@email.com>
2022-05-21 15:37:18 +0300

    refactored

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,23 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 import { ListOfShowsType, endpoints } from '../utils/endpoints';
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 
 import SearchBar from '../components/SearchBar';
 import ShowCard from '../components/ShowCard';
 import { axiosInstance } from '../utils/axiosInstance';
+import { onBottomScreenScroll } from '../utils/onBottomScreenScroll';
 import styles from '../styles/pages/Home.module.scss';
 import { useInfiniteQuery } from 'react-query';
 
-const fetchListOfShows = async (page: string | number) => {
-    return await axiosInstance.get(endpoints(page).listOfShows);
+const fetchListOfShows = async () => {
+    return await axiosInstance.get(endpoints(1).listOfShows);
 };
 
 const Home = () => {
-    const [page, setPage] = useState(1);
     const { data, status, hasNextPage, fetchNextPage, isFetching, isFetchingNextPage } = useInfiniteQuery(
-        ['fetchListOfShows', page],
-        () => fetchListOfShows(page),
+        'fetchListOfShows',
+        fetchListOfShows,
         {
             getNextPageParam: (_lastPage, pages) => {
                 return pages.length + 1;
@@ -26,14 +26,8 @@ const Home = () => {
         }
     );
     useEffect(() => {
-        const onScroll = function () {
-            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-                fetchNextPage();
-            }
-        };
-        window.addEventListener('scroll', onScroll);
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
+        onBottomScreenScroll({ fetchNextPage, hasNextPage });
+    }, [fetchNextPage, hasNextPage]);
 
     return (
         <div className={styles.container}>
@@ -46,12 +40,8 @@ const Home = () => {
                             <Fragment key={idx}>
                                 {group.data.map((show: ListOfShowsType) => {
                                     return (
-                                        <button
-                                            onClick={() => {
-                                                console.log('132123');
-                                            }}
-                                        >
-                                            <ShowCard show={show} key={show.id} />
+                                        <button onClick={() => {}} key={show.id}>
+                                            <ShowCard show={show} />
                                         </button>
                                     );
                                 })}
