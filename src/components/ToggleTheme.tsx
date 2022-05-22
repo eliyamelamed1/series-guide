e982a6bbf8e6f4e22b9feb49e3ee093a48c49459
eliyamelamed1 <eliyamelamed1@email.com>
2022-05-22 18:41:01 +0300

    fixed show card design

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -8,28 +8,16 @@ import { useTheme } from 'next-themes';
 
 const Toggle = () => {
     const { theme, setTheme } = useTheme();
-    const [slide, setSlide] = useState('0%');
-
-    useEffect(() => {
-        if (theme === 'light') return setSlide('0%');
-        setSlide('100%');
-    }, [theme]);
 
     const toggle = () => {
         if (theme === 'light') return setTheme('dark');
         setTheme('light');
     };
     return (
-        <motion.div
-            className={styles.toggle}
-            onClick={toggle}
-            whileHover={{
-                scale: 1.2,
-            }}
-        >
+        <motion.div className={styles.toggle} onClick={toggle}>
             <DarkModeIcon />
             <WbSunnyIcon />
-            <motion.div className={styles.dot} animate={{ x: slide }}></motion.div>
+            <motion.div className={styles.dot} />
         </motion.div>
     );
 };
