a5f26e64a5d06500845268e6d4682d2148f7c192
eliyamelamed1 <eliyamelamed1@email.com>
2022-05-24 17:08:20 +0300

    updated the app favicon in the browser tab

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import AppRouter from './AppRouter';
-import { Helmet } from 'react-helmet';
 import React from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'next-themes';
@@ -14,10 +13,6 @@ const queryClient = new QueryClient();
 export default function App() {
     return (
         <>
-            <Helmet>
-                <meta charSet='utf-8' />
-                <title>Series guide</title>
-            </Helmet>
             <ToastContainer />
             <QueryClientProvider client={queryClient}>
                 <ThemeProvider>
