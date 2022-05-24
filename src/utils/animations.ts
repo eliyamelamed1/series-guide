7639c6a82fc345e7a45107a7d486bc3d04f3ae4f
eliyamelamed1 <eliyamelamed1@email.com>
2022-05-24 16:36:37 +0300

    changed animations

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -4,7 +4,7 @@ export const animations = {
         visible: {
             opacity: 1,
             transition: {
-                staggerChildren: 0.01,
+                staggerChildren: 0.07,
             },
         },
     },
